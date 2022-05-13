27c36ad4c708eeb3e36990a0c17c0787119d8b20
Declan Davis <dexthefish@gmail.com>
2022-05-12 23:24:15 -0400

    wrote code to fetch cat info from thecatapi

---
+++ breedFetcher.js
@@ -0,0 +1,14 @@
+const request = require('request');
+const breed = process.argv[2];
+
+request(`https://api.theaaaaaacatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode);
+  const data = JSON.parse(body)[0];
+  if (typeof data !== "object") {
+    console.log("please enter a valid cat breed.");
+  }
+  if (typeof data === "object") {
+    console.log(data.description);
+  }
+});
