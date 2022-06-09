547798535fd0bf7a9af5447e81283832af114b4d
Declan Davis <dexthefish@gmail.com>
2022-06-09 19:33:52 -0400

    moved more of the request logic into breedFetcherV2 in order to streamline the logic in index.js and the test file

--- breedFetcherV2.js
+++ breedFetcherV2.js
@@ -17,9 +17,13 @@ const fetchBreedDescription = function(breedName, callback) {
 
     const data = JSON.parse(body);
     
-    if (Array.isArray(data)) {
-      return callback(null, data);
+    if (Array.isArray(data) && data.length) {
+      return callback(null, data[0].description);
     }
+    if (Array.isArray(data) && !data.length) {
+      return callback(null, null);
+    }
+
   });
 };
 
--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const { fetchBreedDescription } = require('./breedFetcherV2');
 
 const breedName = process.argv[2];
 
-fetchBreedDescription(breedName, (error, description) => {
+fetchBreedDescription(breedName, (error, desc) => {
   if (error) {
     console.log('Error fetch details:', error);
-  } else if (description.length < 1) {
+  } else if (!desc) {
     console.log('Please enter a valid breed');
-  } else {
-    console.log(description[0].description);
+  } else if (desc) {
+    console.log(desc);
   }
 });
